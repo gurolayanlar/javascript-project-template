ce9c0575207288a23e8843e9e306ac468816493b
Özgün Özdemir <ozgunozdemir@ucuracak.com>
2019-12-06 14:22:30 +0300

    added create react app for client

--- client/README.md
+++ client/README.md
@@ -1,2 +1,3 @@
-# Javascript Project Template Client
-Using ReactJS 
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+Using React JS
--- client/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-//const args = [ 'start' ];
-const args = process.argv.slice(2);
-const opts = { stdio: 'inherit', cwd: 'client', shell: true };
-require('child_process').spawn('yarn', args, opts);
