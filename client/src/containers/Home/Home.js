2a4213611bcc4b54519a0114150b2599f9c6b9d8
OzgunOzdemir <ozgunozdemir92@gmail.com>
2020-01-06 14:24:49 +0300

    added get users queries

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -1,10 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Query } from "react-apollo";
+import { GET_USERS } from '../../queries/index'
 
 class Home extends Component {
     render() {
         return (
             <div>
-               Home Page 
+                <Query query={GET_USERS}>
+                    {
+                        ({data, loading, error}) => {
+                            if (loading) return <div>Loading get users...</div>
+                            if (error) return <div>Error.</div>
+                            return (
+                                <div>
+                                    <ul>
+                                        {
+                                            data.users.map(user => (
+                                                <li>{user.firstName}</li>
+                                            ))
+                                        }
+                                    </ul>
+                                </div>
+                            )
+                        }
+                    }
+                </Query>
             </div>
         )
     }
